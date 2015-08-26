1a6d6b34131447db38eb1fefc90e7701ad8330d6
Kayra <KayraAlat@gmail.com>
2015-08-26 13:35:12 +0100

    Added models

---
+++ Thunder/routines/models.py
@@ -0,0 +1,26 @@
+from __future__ import absolute_import
+from datetime import timedelta
+
+from django.db import models
+from atlas.users.models import User
+
+
+class Routine(models.Model):
+
+    name                = models.CharField("Skill name", max_length=255)
+
+    user                = models.ForeignKey(User)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Exercise(models.Model):
+
+    name                = models.CharField("Skill name", max_length=255)
+    completion_time     = models.DurationField(default=timedelta())
+
+    routine                = models.ForeignKey(Routine)
+
+    def __unicode__(self):
+        return self.name
