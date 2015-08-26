02750eeea38580805328e2d2e00fc755d0733758
Kayra <KayraAlat@gmail.com>
2015-08-26 13:29:57 +0100

    Removed about page

--- config/urls.py
+++ config/urls.py
@@ -9,7 +9,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='pages/home.html'), name="home"),
-    url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name="about"),
 
     # Django Admin
     url(r'^admin/', include(admin.site.urls)),
