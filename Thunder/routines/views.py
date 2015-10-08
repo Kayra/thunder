d40f80c32addc5178bb5ae370c5ad0d408156fbe
Kayra <KayraAlat@gmail.com>
2015-10-08 22:04:05 +0100

    Separated backend and frontend

--- Thunder/routines/views.py
+++
@@ -1,9 +0,0 @@
-from __future__ import absolute_import
-
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-
-
-@login_required
-def routineView(request):
-    return render(request, 'routines/routine.html')
--- Thunder/static/js/project.js
+++
@@ -1,8 +0,0 @@
-/* Angular settings */
-
-$interpolateProvider.startSymbol('[[').endSymbol(']]');
-
-$httpProvider.default.xsrfCookieName = 'csrftoken';
-$httpProvider.default.xsrfHeaderName = 'X-CSRFToken';
-
-$resourceProvider.defaults.stripTrailingSlashes = false;
