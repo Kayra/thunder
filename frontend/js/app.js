5040c100c31e8c8a89214d6d2fabda615138cd5b
Kayra <KayraAlat@gmail.com>
2015-10-11 19:50:51 +0100

    Added all pages to angular router

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -32,10 +32,25 @@
             templateUrl: 'partials/list.html',
             controller: 'RoutineListController',
         })
-        .state('add', {
-            url: '/add',
+        .state('add_routine', {
+            url: '/add_routine',
             templateUrl: 'partials/add_routine.html',
             controller: 'RoutineAddRoutineController',
+        })
+        .state('add_exercises', {
+            url: '/add_exercises',
+            templateUrl: 'partials/add_exercises.html',
+            controller: 'RoutineAddExercisesController',
+        })
+        .state('edit', {
+            url: '/edit',
+            templateUrl: 'partials/edit.html',
+            controller: 'RoutineEditController',
+        })
+        .state('use', {
+            url: '/use',
+            templateUrl: 'partials/use.html',
+            controller: 'RoutineUseController',
         });
 
     $urlRouterProvider.otherwise('/');
