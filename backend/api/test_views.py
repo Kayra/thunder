576acb01323cfa28cd99b4099973294efaf088e3
kayra <KayraAlat@gmail.com>
2015-12-21 23:43:31 +0000

    Server - Added test structure

---
+++ backend/api/test_views.py
@@ -0,0 +1,27 @@
+from django.test import TestCase
+
+class RoutineAPITests(TestCase):
+
+	def setUp(self):
+		pass
+
+	def test_getRoutines(self):
+		pass
+
+	def test_getRoutine(self):
+		pass
+
+	def test_postRoutines(self):
+		pass
+
+	def test_deleteRoutine(self):
+		pass
+
+	def test_postExercises(self):
+		pass
+
+	def test_postExercise(self):
+		pass
+
+	def test_deleteExercise(self):
+		pass
