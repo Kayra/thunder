218710748835dcdcd17359aff8cc169a01c93d3b
kayra <KayraAlat@gmail.com>
2015-12-28 22:39:31 +0000

    Server - Updated routines URL naming

--- server/routines/urls.py
+++ server/routines/urls.py
@@ -4,18 +4,18 @@ from . import views
 
 urlpatterns = [
 
-    url(r'^get/routines$', views.getRoutines, name='get_routines'),
+    url(r'^get-routines', views.getRoutines, name='get_routines'),
 
-    url(r'^get/routine$', views.getRoutine, name='get_routine'),
+    url(r'^get-routine', views.getRoutine, name='get_routine'),
 
-    url(r'^post/routine$', views.postRoutine, name='post_routine'),
+    url(r'^create-routine', views.postRoutine, name='create_routine'),
 
-    url(r'^post/routine-delete$', views.postRoutineDelete, name='post_routine_delete'),
+    url(r'^delete-routine', views.postRoutineDelete, name='delete_routine'),
 
-    url(r'^post/exercises$', views.postExercises, name='post_exercises'),
+    url(r'^create-exercises', views.postExercises, name='create_exercises'),
 
-    url(r'^post/exercise$', views.postExercise, name='post_exercise'),
+    url(r'^create-exercise', views.postExercise, name='create_exercise'),
 
-    url(r'^post/exercise-delete$', views.postExerciseDelete, name='post_exercise_delete'),
+    url(r'^delete-exercise', views.postExerciseDelete, name='delete_exercise'),
 
 ]
