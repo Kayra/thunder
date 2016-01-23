34d9a85edb6697278d60fda6c6011d9269da52dd
Kayra <KayraAlat@gmail.com>
2016-01-23 18:07:15 +0000

    Server - Added utilities test structure

---
+++ server/routines/test_utilities.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+
+from .models import Routine, Exercise
+
+
+class RoutineUtilitiesTests(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_calculateTotalTime(self):
+        pass
+
+    def test_updateTotalTime(self):
+        pass
