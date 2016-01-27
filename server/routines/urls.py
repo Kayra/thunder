55034428925911b0897709e22e21eddc9c9edeef
Kayra <KayraAlat@gmail.com>
2016-01-27 03:18:20 +0000

    Server - Removed redundant urls

--- server/routines/urls.py
+++ server/routines/urls.py
@@ -4,16 +4,6 @@ from routines import views
 
 urlpatterns = [
 
-    url(r'^get-full-routine', views.getFullRoutine, name='get_full_routine'),
-
-    url(r'^create-exercises', views.createExercises, name='create_exercises'),
-
-    url(r'^create-exercise', views.createExercise, name='create_exercise'),
-
-    url(r'^edit-exercise', views.editExercise, name='edit_exercise'),
-
-    url(r'^delete-exercise', views.deleteExercise, name='delete_exercise'),
-
     url(r'^routines/$', views.RoutineList.as_view()),
 
     url(r'^routines/(?P<pk>[0-9]+)/$', views.RoutineDetail.as_view()),
