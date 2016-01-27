7065bf3bca47b1d175459837b99b1a4d6ddfedde
Kayra <KayraAlat@gmail.com>
2016-01-27 02:13:57 +0000

    Server - Removed redundant routine code

--- server/routines/urls.py
+++ server/routines/urls.py
@@ -4,17 +4,7 @@ from routines import views
 
 urlpatterns = [
 
-    url(r'^get-routines', views.getRoutines, name='get_routines'),
-
-    url(r'^get-routine', views.getRoutine, name='get_routine'),
-
     url(r'^get-full-routine', views.getFullRoutine, name='get_full_routine'),
-
-    url(r'^create-routine', views.createRoutine, name='create_routine'),
-
-    url(r'^edit-routine', views.editRoutine, name='edit_routine'),
-
-    url(r'^delete-routine', views.deleteRoutine, name='delete_routine'),
 
     url(r'^create-exercises', views.createExercises, name='create_exercises'),
 
--- server/routines/views.py
+++ server/routines/views.py
@@ -23,37 +23,6 @@ class RoutineDetail(generics.RetrieveUpdateDestroyAPIView):
 
 
 @api_view(['GET'])
-def getRoutines(request):
-
-    try:
-        routines = Routine.objects.all()
-    except Routine.DoesNotExist:
-        return Response(status=status.HTTP_400_BAD_REQUEST)
-
-    serializer = RoutineSerializer(routines, many=True)
-    return Response(serializer.data)
-
-
-@api_view(['GET'])
-def getRoutine(request):
-
-    try:
-        identifier = request.query_params['id']
-    except MultiValueDictKeyError:
-        return Response(status=status.HTTP_400_BAD_REQUEST)
-
-    try:
-        routine = Routine.objects.get(pk=identifier)
-    except Routine.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    except TypeError:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    serializedRoutine = RoutineSerializer(routine)
-    return Response(serializedRoutine.data)
-
-
-@api_view(['GET'])
 def getFullRoutine(request):
 
     try:
@@ -64,55 +33,6 @@ def getFullRoutine(request):
     exercises = Exercise.objects.filter(routine__id=identifier).order_by('position')
     fullSerializer = FullRoutineSerializer(exercises, many=True)
     return Response(fullSerializer.data)
-
-
-@api_view(['POST'])
-def createRoutine(request):
-
-    routineSerializer = RoutineSerializer(data=request.data)
-
-    if routineSerializer.is_valid():
-        routineSerializer.save()
-        return Response(routineSerializer.data)
-
-    else:
-        return Response(routineSerializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(['PUT'])
-def editRoutine(request):
-
-    try:
-        routine = Routine.objects.get(pk=request.data['id'])
-        serializedRoutine = RoutineSerializer(routine, data=request.data)
-    except MultiValueDictKeyError:
-        serializedRoutine = RoutineSerializer(data=request.data)
-    except Routine.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    if serializedRoutine.is_valid():
-        serializedRoutine.save()
-        return Response(status=status.HTTP_200_OK)
-    else:
-        return Response(serializedRoutine.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(['DELETE'])
-def deleteRoutine(request):
-
-    #  Handle both query params and data to satisfy the lack of query params in test client
-    try:
-        identifier = request.query_params['id']
-    except MultiValueDictKeyError:
-        identifier = request.data['id']
-
-    try:
-        routine = Routine.objects.get(pk=identifier)
-    except Routine.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    routine.delete()
-    return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 @api_view(['POST'])
