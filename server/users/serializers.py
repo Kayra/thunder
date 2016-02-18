fa83c19af8234dd5130184fd69627e326570fa55
Kayra <KayraAlat@gmail.com>
2016-02-18 23:39:31 +0000

    Server - Removed need to include routines when creating user

--- server/users/serializers.py
+++ server/users/serializers.py
@@ -2,12 +2,9 @@ from django.contrib.auth.models import User
 
 from rest_framework import serializers
 
-from routines.models import Routine
-
 
 class UserSerializer(serializers.ModelSerializer):
-    routines = serializers.PrimaryKeyRelatedField(many=True, queryset=Routine.objects.all())
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'routines')
+        fields = ('id', 'username', 'email', 'password')
