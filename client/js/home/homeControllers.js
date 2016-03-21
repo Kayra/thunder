8cdba8accbb0826f738fc28990897e078078e318
Kayra <KayraAlat@gmail.com>
2016-03-21 23:26:22 +0000

    Client - Added home page

---
+++ client/js/home/homeControllers.js
@@ -0,0 +1,11 @@
+(function() {
+
+    var homeControllers = angular.module('routineApp.homeControllers', []);
+
+    homeControllers.controller('HomeController', function() {
+
+        var vm = this;
+
+    });
+
+})();
