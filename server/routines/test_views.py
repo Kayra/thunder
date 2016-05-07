2089c949da106aad5d3e39d8ad43f8563a4083f7
Kayra <KayraAlat@gmail.com>
2016-05-07 21:31:17 +0100

    Server - Added minimum test structure

--- server/routines/test_views.py
+++ server/routines/test_views.py
@@ -36,152 +36,33 @@ class RoutineAPITests(TestCase):
         for index, exerciseName in enumerate(exerciseNames, start=1):
             Exercise.objects.create(name=exerciseName, completion_time='00:01:00', position=index, routine=testRoutine2)
 
-    def test_getRoutines(self):
-
-        """
-            All routines should be returned
-        """
-
-        url = reverse('routines:get_routines')
-
-        response = self.client.get(url)
-        self.assertEquals(response.status_code, 200)  # Make sure valid request returns success response
-
-        routinesFromDB = Routine.objects.all()
-        for routine in response.data:
-            self.assertTrue(routinesFromDB.filter(pk=routine['id']).exists())  # Make sure routines from API match routines in routinesFromDb
-
-    def test_getRoutine(self):
-
-        """
-            A routine and associated exercises should be returned when a correct id is passed as a parameter
-        """
-
-        url = reverse('routines:get_full_routine')
-
-        response = self.client.get(url, {'wrong': 'wrong'})
-        self.assertEquals(response.status_code, 400)  # Make sure bad params return error response
-
-        routineFromDB = Routine.objects.all()[:1].get()
-        response = self.client.get(url, {'id': routineFromDB.id})
-        self.assertEquals(response.status_code, 200)  # Make sure valid request returns success response
-
-        routineId = response.data[0]['routine']['id']
-        self.assertEquals(routineFromDB.id, routineId)  # Make sure the correct routine was returned
-
-    def test_getFullRoutine(self):
+    def test_get_routine_list(self):
         pass
 
-    def test_createRoutine(self):
-
-        """
-            A routine created with the API should be in the database
-        """
-
-        url = reverse('routines:create_routine')
-
-        response = self.client.post(url)
-        self.assertEquals(response.status_code, 400)  # Make sure no params return error response
-
-        response = self.client.post(url, {'wrong': 'wrong'})
-        self.assertEquals(response.status_code, 400)  # Make sure bad params return error response
-
-        routineToCreate = {}
-        routineToCreate['name'] = "test3"
-        response = self.client.post(url, routineToCreate)
-        self.assertEquals(response.status_code, 200)  # Make sure valid request returns success response
-
-        routineFromDB = Routine.objects.get(pk=response.data['id'])
-        self.assertEquals(routineFromDB.name, response.data['name'])  # Make sure the routine created via the API is in the DB
-
-    def test_editRoutine(self):
+    def test_get_routine(self):
         pass
 
-    def test_deleteRoutine(self):
-
-        """
-            A routine deleted with the API should not be in the database
-        """
-
-        url = reverse('routines:delete_routine')
-
-        response = self.client.delete(url, {'wrong': 'wrong'}, content_type='application/json')
-        self.assertEquals(response.status_code, 400)  # Make sure bad parameters return error response
-
-        routineToDelete = Routine.objects.all()[:1].get()
-        routineToDeleteJson = json.dumps({'id': routineToDelete.id})
-        response = self.client.delete(url, routineToDeleteJson, content_type='application/json')
-        self.assertEquals(response.status_code, 204)  # Make sure valid request returns success response
-
-        with self.assertRaises(Routine.DoesNotExist):
-            Routine.objects.get(pk=routineToDelete.id)  # Make sure the routine no longer exists in the DB
-
-    def test_createExercises(self):
-
-        """
-            All exercises created with the API should be in the database
-        """
-
-        url = reverse('routines:create_exercises')
-
-        response = self.client.post(url)
-        self.assertEquals(response.status_code, 400)  # Make sure no params return error response
-
-        response = self.client.post(url, {'wrong': 'wrong'})
-        self.assertEquals(response.status_code, 400)  # Make sure bad params return error response
-
-        exercisesToCreate = []
-        for index in range(0, 3):
-            exercisesToCreate.append(createExerciseForAPI(index))
-        exercisesToCreateJson = json.dumps(exercisesToCreate)
-        response = self.client.post(url, exercisesToCreateJson, content_type='application/json')
-        self.assertEquals(response.status_code, 200)  # Make sure valid request returns success response
-
-        for exercise in response.data:
-            self.assertEquals(Exercise.objects.filter(pk=exercise['id']).count(), 1)  # Make sure the exercises created via the API are in the DB
-
-    def test_createExercise(self):
-
-        """
-            An exercise created with the API should be in the database
-        """
-
-        url = reverse('routines:create_exercise')
-
-        response = self.client.post(url)
-        self.assertEquals(response.status_code, 400)  # Make sure no params return error response
-
-        response = self.client.post(url, {'wrong': 'wrong'})
-        self.assertEquals(response.status_code, 400)  # Make sure bad params return error response
-
-        exerciseToCreate = createExerciseForAPI()
-        response = self.client.post(url, exerciseToCreate)
-        self.assertEquals(response.status_code, 200)  # Make sure valid request returns success response
-
-        exerciseFromDB = Exercise.objects.get(name=exerciseToCreate['name'], routine__id=exerciseToCreate['routine'], position=exerciseToCreate['position'])
-        self.assertEquals(exerciseFromDB.id, response.data['id'])  # Make sure the exercise created via the API is in the DB
-
-    def text_editExercise(self):
+    def test_create_routine(self):
         pass
 
-    def test_deleteExercise(self):
+    def test_edit_routine(self):
+        pass
 
-        """
-            An exercise deleted with the API should not be in the database
-        """
+    def test_delete_routine(self):
+        pass
 
-        url = reverse('routines:delete_exercise')
+    def test_get_exercise_list(self):
+        pass
 
-        response = self.client.delete(url)
-        self.assertEquals(response.status_code, 400)  # Make sure no params return error response
+    def test_get_exercise(self):
+        pass
 
-        response = self.client.delete(url, {'wrong': 'wrong'}, content_type='application/json')
-        self.assertEquals(response.status_code, 400)  # Make sure bad parameters return error response
+    def test_create_exercise(self):
+        pass
 
-        exerciseToDelete = Exercise.objects.all()[:1].get()
-        exerciseToDeleteJson = json.dumps({'id': exerciseToDelete.id, 'routine': exerciseToDelete.routine.id})
-        response = self.client.delete(url, exerciseToDeleteJson, content_type='application/json')
-        self.assertEquals(response.status_code, 202)  # Make sure valid request returns success response
+    def test_edit_exercise(self):
+        pass
 
-        with self.assertRaises(Exercise.DoesNotExist):
-            Exercise.objects.get(pk=exerciseToDelete.id)  # Make sure the exercise no longer exists in the DB
+    def test_delete_exercise(self):
+        pass
+
