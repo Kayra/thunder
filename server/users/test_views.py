3b70ad5221cd508dc5cdbf67925173668b12c262
Kayra <KayraAlat@gmail.com>
2016-05-08 01:58:36 +0100

    Server - Added minimum user test structure

---
+++ server/users/test_views.py
@@ -0,0 +1,26 @@
+import json
+
+from django.test import TestCase
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
+
+
+class RoutineAPITests(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_get_user(self):
+        pass
+
+    def test_create_user(self):
+        pass
+
+    def test_edit_user(self):
+        pass
+
+    def test_delete_user(self):
+        pass
+
+    def test_auth_user(self):
+        pass
