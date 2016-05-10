f2447e4dc2d84186690fb833294423d57e0b0a32
Kayra <KayraAlat@gmail.com>
2016-05-10 15:35:25 +0100

    Server - Added user tests

--- server/users/test_views.py
+++ server/users/test_views.py
@@ -1,26 +1,142 @@
 import json
 
-from django.test import TestCase
-from django.core.urlresolvers import reverse
+from django.core.urlresolvers import reverse, NoReverseMatch
 from django.contrib.auth.models import User
+from django.test import TestCase
+
+from rest_framework_jwt.settings import api_settings
 
 
-class RoutineAPITests(TestCase):
+class UserAPITests(TestCase):
 
     def setUp(self):
-        testUser = User.objects.create(username='test', email='test@test.com', password='test123')
+        user = User(username="initial_tester", email="initial_tester@test.com")
+        user.set_password("initial_tester123")
+        user.save()
 
-    def test_get_user(self):
-        pass
+        self.edited_user = {
+            'id': user.id,
+            'username': 'edited',
+            'email': user.email
+        }
 
-    def test_create_user(self):
-        pass
+        self.client.force_login(user=user)
 
-    def test_edit_user(self):
-        pass
+    def test_get_user_no_parameters(self):
+        with self.assertRaises(NoReverseMatch):
+            reverse('users:user_detail')
 
-    def test_delete_user(self):
-        pass
+    def test_get_user_invalid_parameters(self):
+        with self.assertRaises(NoReverseMatch):
+            reverse('users:user_detail', kwargs={'invalid': 'invalid'})
 
-    def test_auth_user(self):
-        pass
+    def test_get_user_doesnt_exist(self):
+        url = reverse('users:user_detail', kwargs={'pk': '959'})
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 404)
+
+    def test_get_user_does_exist(self):
+
+        user = User.objects.get(username='initial_tester')
+
+        url = reverse('users:user_detail', kwargs={'pk': user.pk})
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_create_user_invalid_parameters(self):
+        url = reverse('users:user_create')
+        response = self.client.post(url, {'invalid': 'invalid'})
+        self.assertEquals(response.status_code, 400)
+
+    def test_create_user_valid_parameters(self):
+
+        test_user = {
+            'username': 'tester',
+            'email': 'tester@test.com',
+            'password': 'tester123'
+        }
+
+        url = reverse('users:user_create')
+        response = self.client.post(url, test_user)
+        self.assertEquals(response.status_code, 201)
+        self.assertTrue(User.objects.get(username=test_user['username']))
+
+    def test_update_user_no_parameters(self):
+        with self.assertRaises(NoReverseMatch):
+            reverse('users:user_detail')
+
+    def test_update_user_invalid_parameters(self):
+        with self.assertRaises(NoReverseMatch):
+            reverse('users:user_detail', kwargs={'invalid': 'invalid'})
+
+    def test_update_user_nonexistant_pk(self):
+        url = reverse('users:user_detail', kwargs={'pk': '959'})
+        response = self.client.patch(url, self.edited_user)
+        self.assertEquals(response.status_code, 404)
+        response = self.client.put(url, self.edited_user)
+        self.assertEquals(response.status_code, 404)
+
+    def test_update_user_valid_parameters_patch(self):
+        url = reverse('users:user_detail', kwargs={'pk': self.edited_user['id']})
+        response = self.client.patch(url, json.dumps(self.edited_user), content_type='application/json')
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(response.data['id'], User.objects.get(username=self.edited_user['username']).id)
+
+    def test_update_user_valid_parameters_put(self):
+
+        edited_user_put = self.edited_user
+        edited_user_put['password'] = User.objects.get(id=edited_user_put['id']).password
+
+        url = reverse('users:user_detail', kwargs={'pk': edited_user_put['id']})
+        response = self.client.put(url, json.dumps(edited_user_put), content_type='application/json')
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(response.data['id'], User.objects.get(username=edited_user_put['username']).id)
+
+    def test_delete_user_no_parameters(self):
+        with self.assertRaises(NoReverseMatch):
+            reverse('users:user_detail')
+
+    def test_delete_user_invalid_parameters(self):
+        with self.assertRaises(NoReverseMatch):
+            reverse('users:user_detail', kwargs={'invalid': 'invalid'})
+
+    def test_delete_user_valid_parameters(self):
+
+        test_user = User.objects.get(username='initial_tester')
+
+        url = reverse('users:user_detail', kwargs={'pk': test_user.pk})
+        response = self.client.delete(url)
+        self.assertEquals(response.status_code, 204)
+        with self.assertRaises(User.DoesNotExist):
+            User.objects.get(username='initial_tester')
+
+    def test_user_auth_no_params(self):
+        url = reverse('users:user_auth')
+        response = self.client.post(url)
+        self.assertEquals(response.status_code, 400)
+
+    def test_user_auth_invalid_params(self):
+        url = reverse('users:user_auth')
+        response = self.client.post(url, {'invalid': 'invalid'})
+        self.assertEquals(response.status_code, 400)
+
+    def test_user_auth_invalid_login(self):
+        url = reverse('users:user_auth')
+        response = self.client.post(url, {'username': 'invalid'})
+        self.assertEquals(response.status_code, 400)
+
+    def test_user_auth_valid_login(self):
+        url = reverse('users:user_auth')
+        response = self.client.post(url, {'username': 'initial_tester', 'password': 'initial_tester123'})
+        self.assertEquals(response.status_code, 200)
+        self.assertIn('token', response.data)
+
+    def test_user_auth_token(self):
+        user = User.objects.get(username='initial_tester')
+        jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+        jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+        payload = jwt_payload_handler(user)
+
+        url = reverse('users:user_auth')
+        response = self.client.post(url, {'username': 'initial_tester', 'password': 'initial_tester123'})
+        self.assertEquals(jwt_encode_handler(payload), response.data['token'])
--- server/users/urls.py
+++ server/users/urls.py
@@ -5,11 +5,11 @@ from users import views
 
 urlpatterns = [
 
-    url(r'^users/$', views.UserCreate.as_view()),
+    url(r'^users/$', views.UserCreate.as_view(), name='user_create'),
 
-    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
+    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view(), name='user_detail'),
 
-    url(r'^user-auth/', obtain_jwt_token),
+    url(r'^user-auth/', obtain_jwt_token, name='user_auth'),
 
     url(r'^token-refresh/', refresh_jwt_token),
 
